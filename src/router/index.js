b9f09d26c026034913e617800e9ddca6a64ea4c4
Darren Petersen <91587619+Darrenpet@users.noreply.github.com>
2022-05-16 09:01:22 +0200

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,88 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import MusicBlogs from "../views/MusicBlogs.vue";
-import TechBlogs from "../views/TechBlogs.vue";
-import LifestyleBlogs from "../views/LifestyleBlogs.vue";
-import GamingBlogs from "../views/GamingBlogs.vue";
-import PostDetails from "../views/PostDetails.vue";
 
 const routes = [
   {
     path: "/",
-    name: "Home",
+    name: "home",
     component: HomeView,
   },
   {
-    path: "/Musicblogs",
-    name: "MusicBlogs",
-    component: MusicBlogs,
+    path: "/signin",
+    name: "signin",
+    component: () =>
+      import(/* webpackChunkName: "signin" */ "../views/SignIn.vue"),
   },
   {
-    path: "/Lifestyleblogs",
-    name: "LifestyleBlogs",
-    component: LifestyleBlogs,
+    path: "/signup",
+    name: "signup",
+    component: () => import(/* webpackChunkName: "signup" */ "../views/SignUp"),
   },
   {
-    path: "/Gamingblogs",
-    name: "GamingBlogs",
-    component: GamingBlogs,
+    path: "/gamingblogs",
+    name: "gamingblogs",
+    component: () =>
+      import(/* webpackChunkName: "gamingblogs" */ "../views/GamingBlogs.vue"),
   },
   {
-    path: "/Techblogs",
-    name: "TechBlogs",
-    component: TechBlogs,
+    path: "/lifestyleblogs",
+    name: "lifestyleblogs",
+    component: () =>
+      import(
+        /* webpackChunkName: "lifestyleblogs" */ "../views/LifestyleBlogs.vue"
+      ),
   },
   {
-    path: "/post/:id",
-    name: "PostDetails",
-    component: PostDetails,
+    path: "/musicblogs",
+    name: "musicblogs",
+    component: () =>
+      import(/* webpackChunkName: "musicblogs" */ "../views/MusicBlogs.vue"),
+  },
+  {
+    path: "/techblogs",
+    name: "techblogs",
+    component: () =>
+      import(/* webpackChunkName: "techblogs" */ "../views/TechBlogs.vue"),
+  },
+  {
+    path: "/gamingblogs/:id",
+    name: "gamingdetails",
+    component: () =>
+      import(
+        /* webpackChunkName: "gamingdetails" */ "../views/GamingDetails.vue"
+      ),
+    props: true,
+  },
+  {
+    path: "/lifestyleblogs/:id",
+    name: "lifestyledetails",
+    component: () =>
+      import(
+        /* webpackChunkName: "lifestyledetails" */ "../views/LifestyleDetails.vue"
+      ),
+    props: true,
+  },
+  {
+    path: "/musicblogs/:id",
+    name: "musicdetails",
+    component: () =>
+      import(
+        /* webpackChunkName: "musicdetails" */ "../views/MusicDetails.vue"
+      ),
+    props: true,
+  },
+  {
+    path: "/techblogs/:id",
+    name: "techdetails",
+    component: () =>
+      import(/* webpackChunkName: "techdetails" */ "../views/TechDetails.vue"),
+    props: true,
+  },
+  {
+    path: "/profilepage",
+    name: "profilepage",
+    component: () =>
+      import(/* webpackChunkName: "profilepage" */ "../views/ProfilePage.vue"),
   },
 ];
 
